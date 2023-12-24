69499689e0aab35a100b57eae6e1d798ddffcd9e
Super-Kenil <kenilsudani.blog@gmail.com>
2023-12-25 00:06:05 +0530

    FIX: remote patterns + favicon

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.builder.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.builder.io'
+      }
+    ],
     dangerouslyAllowSVG: true,
   }
 }
